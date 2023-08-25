48057bee25737d67d13720e8ffb68cf200b924ac
GUSTAVO CALDAS SILVEIRA BORGES <gustavo.borges@a.ucb.br>
2023-08-25 08:53:02 -0300

    Atividade: Regressao Linear
    Atividade realizada em C para matéria de Estrutura de Dados na UCB,
    Universidade Católica de Brasília, aula ministrada pelo Professor: João
    Pedro Macleure Nunes dos Santos.

---
+++ atividade3/regressao_linear.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Objeto de cada ponto
+typedef struct {
+    int x;
+    float y;
+} Ponto;
+
+/*
+fonte: http://mtm.ufsc.br/~azeredo/cursoC/aulas/c790.html#:~:text=int%20main%20(int%20argc%2Cchar,chamada%20na%20linha%20de%20comando
+Inicializando a main com parâmetros, onde:
+
+Argc = Argument count, é um int e possui o número de argumentos com os quais a função (main) foi chamada na linha de comando.
+É no mínimo 1, porque o nome do programa é contado como sendo o primeiro argumento.
+
+Argv = Argument values, é um ponteiro (*) para uma matriz de strings. Cada string desta matriz é um dos parâmetros da linha de comando.
+O argv[0] sempre aponta para o nome do programa.
+*/ 
+int main(int argc, char *argv[]){
+
+    // Declarando variáveis
+    float mediaX, mediaY, inclinacao, intercepcao;
+    
+
+
+    return 0;
+}
